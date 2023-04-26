5134f8f306335db149425463c70fddf61f6a2184
Camille <camille.bedeche@laposte.net>
2023-04-26 15:03:51 +0200

    meta description

--- size.js
+++ size.js
@@ -11,4 +11,28 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
     });
+
+    const normalSize = document.querySelector('#size-normal');
+
+    normalSize.addEventListener('click', function() {
+
+        const paragraphs = document.querySelectorAll('p');
+
+        paragraphs.forEach(function(paragraph) {
+            paragraph.style.fontSize = '18px';
+        });
+
+    });
+
+    const litlleSize = document.querySelector('#size-little');
+
+    litlleSize.addEventListener('click', function() {
+
+        const paragraphs = document.querySelectorAll('p');
+
+        paragraphs.forEach(function(paragraph) {
+            paragraph.style.fontSize = '14px';
+        });
+
+    });
 });
